c574ab3ac6a60fa73d1669f7d2bc618d5e5fb3ab
David Pongo <david.pongo@gmail.com>
2023-04-12 17:39:06 +0100

    Switching off chrome web securiry due to saucedemo.com not accepting cross-origin remote access

--- cypress/integration/testLoginForm/testLoginForm.spec.js
+++ cypress/integration/testLoginForm/testLoginForm.spec.js
@@ -6,11 +6,12 @@ Given ('login form is displayed', () => {
     loginPage.main_container.should('be.visible');
 })
 When ('{string} has been entered', (username) => {
-
+    loginPage.username.type(username);
 })
 And ('{string} is entered', (password) => {
-
+    loginPage.password.type(password);
 })
 Then ('user has successfully logged in using {string} with {string}', (username, password) => {
-
+    loginPage.loginButton.click();
+    cy.url().should('include', '/inventory.html')
 })
